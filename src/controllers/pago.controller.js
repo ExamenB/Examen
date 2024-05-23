96974433e4e11354977876797dad03028cb2b769
Esneider Giraldo <xxsilentytbey@gmail.com>
2024-05-23 09:15:24 -0500

    Funciona el metodo POST, GET, GET, PUT y DELETE

--- src/controllers/pago.controller.js
+++ src/controllers/pago.controller.js
@@ -1,27 +1,56 @@
 import { pool } from "../db.js";
 
 export const crear = async (req, res) => {
-    const {valor, tipo} = req.body;
-    const [rows] = await pool.query('CALL sp_crear(?, ?)', [valor, tipo])
-    res.json(rows)
+    try {
+        const {valor, tipo} = req.body;
+        const [rows] = await pool.query('CALL sp_crear(?, ?)', [valor, tipo])
+        res.json(rows)
+    } catch (error) {
+        res.json({"Error":error})
+    }
+
 }
 
 export const listar = async (req, res) => {
-    const [rows] = await pool.query('CALL sp_listar()')
-    res.json(rows[0])
+    try {
+        const [rows] = await pool.query('CALL sp_listar()')
+        res.json(rows[0])
+    } catch (error) {
+        res.json({"Error":error})
+    }
+
 }
 
 export const mostrar1 = async (req, res) => {
-    const {id} = req.params;
-    const [rows] = await pool.query('CALL sp_mostrar1(?)', [id])
-    res.json(rows[0])
+    try {
+        const {id} = req.params;
+        const [rows] = await pool.query('CALL sp_mostrar1(?)', [id])
+        res.json(rows[0])
+    } catch (error) {
+        res.json({"Error":error})
+    }
+}
+
+export const editar = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {valor, tipo} = req.body;
+        const [rows] = await pool.query('CALL sp_editar(?, ?, ?)', [id, valor, tipo])
+        res.json(rows)
+    } catch (error) {
+        res.json({"Error":error})
+    }
+}
+
+export const eliminar = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const [rows] = await pool.query('CALL sp_elim(?)', [id])
+        res.json({"Ha sido eliminada el pago con la id":id})
+    } catch (error) {
+        res.json({"Error":error})
+    }
 }
 
 
 
-// export const editar = async (req, res) => {
-//     const id = req.params;
-//     const {valor, tipo} = req.body;
-//     const [rows] = await pool.query('CALL sp_editar(?, ?)', [valor, tipo])
-//     res.json(rows)
-// }
--- src/routes/pago.routes.js
+++ src/routes/pago.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { crear, listar, mostrar1 } from "../controllers/pago.controller.js";
+import { crear, eliminar, listar, editar, mostrar1 } from "../controllers/pago.controller.js";
 
 const router = Router();
 
 router.get("/", listar)
 router.get("/:id", mostrar1)
 router.post("/", crear)
-
-
-// router.put("/:id", editar)
+router.put("/:id", editar)
+router.delete("/:id", eliminar)
 
 export default router;
