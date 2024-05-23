cd1c55e83bba2fe730b87f60b0947bd9f2ac89b0
Esneider Giraldo <xxsilentytbey@gmail.com>
2024-05-23 08:56:13 -0500

    Funciona el metodo POST, GET, GET

--- src/controllers/pago.controller.js
+++ src/controllers/pago.controller.js
@@ -5,3 +5,23 @@ export const crear = async (req, res) => {
     const [rows] = await pool.query('CALL sp_crear(?, ?)', [valor, tipo])
     res.json(rows)
 }
+
+export const listar = async (req, res) => {
+    const [rows] = await pool.query('CALL sp_listar()')
+    res.json(rows[0])
+}
+
+export const mostrar1 = async (req, res) => {
+    const {id} = req.params;
+    const [rows] = await pool.query('CALL sp_mostrar1(?)', [id])
+    res.json(rows[0])
+}
+
+
+
+// export const editar = async (req, res) => {
+//     const id = req.params;
+//     const {valor, tipo} = req.body;
+//     const [rows] = await pool.query('CALL sp_editar(?, ?)', [valor, tipo])
+//     res.json(rows)
+// }
--- src/routes/pago.routes.js
+++ src/routes/pago.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { crear } from "../controllers/pago.controller.js";
+import { crear, listar, mostrar1 } from "../controllers/pago.controller.js";
 
 const router = Router();
 
+router.get("/", listar)
+router.get("/:id", mostrar1)
 router.post("/", crear)
+
+
+// router.put("/:id", editar)
 
 export default router;
